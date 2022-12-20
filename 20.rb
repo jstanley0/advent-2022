f32ce5291aea511ec7f3a8c6661565b6ef312441
Jeremy Stanley <jeremy@instructure.com>
2022-12-19 22:42:01 -0700

    20

---
+++ 20.rb
@@ -0,0 +1,14 @@
+data = ARGF.readlines.map(&:to_i)
+data = data.each_with_index.map { |n, i| [n, i] }
+
+data.size.times do |n|
+  i = data.find_index {_2 == n}
+  v = data.delete_at(i)
+  data.insert((i + v[0]) % data.size, v)
+end
+
+i = data.find_index { _1[0] == 0 }
+x = data[(i + 1000) % data.size][0]
+y = data[(i + 2000) % data.size][0]
+z = data[(i + 3000) % data.size][0]
+p x, y, z, x+y+z
