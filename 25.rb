65b152a5b223d2a955fceba3ba98bd6a74656011
Jeremy Stanley <jeremy@instructure.com>
2022-12-25 12:05:32 -0700

    save six bytes!

--- 25.rb
+++ 25.rb
@@ -1,2 +1,2 @@
 n=$<.sum{|t|t.chop.chars.reduce(0){_1*5+'=-012'.index(_2)-2}}
-s='';while n>0;r=n%5;n=n/5+(r>2?1:0);s=r.to_s+s;end;puts s.tr'34','=-'
+s='';while n>0;r=n%5;n=n/5+r/3;s=r.to_s+s;end;puts s.tr'34','=-'
