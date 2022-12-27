05504e7389f2882151f96975017f67345f4fc12e
Jeremy Stanley <jeremy@instructure.com>
2022-12-27 13:54:02 -0700

    fifteen bytes

--- 25.rb
+++ 25.rb
@@ -1,2 +1,2 @@
-n=$<.sum{|t|t.chop.chars.reduce(0){_1*5+'=-012'.index(_2)-2}}
-s='';while n>0;r=n%5;n=n/5+r/3;s=r.to_s+s;end;puts s.tr'34','=-'
+d='=-012';n=$<.sum{|t|t.chop.chars.reduce(0){_1*5+d.index(_2)-2}}
+s='';while n>0;n+=2;s=d[n%5]+s;n/=5;end;$><<s
